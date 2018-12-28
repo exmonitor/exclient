cb47c1ff29d3b7c7dd46c24e8c682fa191cc92f4
Vaclav Rozsypalek <vaclav@giantswarm.io>
2018-12-28 11:55:12 +0100

    add conenction string

--- database/multi/multi.go
+++ database/multi/multi.go
@@ -4,20 +4,20 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/exmonitor/chronos"
 	"github.com/exmonitor/exlogger"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/pkg/errors"
 	"github.com/olivere/elastic"
-	"github.com/exmonitor/chronos"
+	"github.com/pkg/errors"
 
-	"github.com/exmonitor/exclient/database"
 	"context"
+	"github.com/exmonitor/exclient/database"
 )
 
 const (
 	sqlDriver = "mysql"
 
-	esStatusIndex="service_status"
+	esStatusIndex = "service_status"
 )
 
 func DBDriverName() string {
@@ -39,10 +39,10 @@ type Config struct {
 }
 
 type Client struct {
-	esClient *elastic.Client
+	esClient  *elastic.Client
 	sqlClient *sql.DB
 
-	ctx context.Context
+	ctx           context.Context
 	logger        *exlogger.Logger
 	timeProfiling bool
 	// implement client db interface
@@ -70,19 +70,20 @@ func New(conf Config) (*Client, error) {
 	t1.Finish()
 	conf.Logger.Log("successfully connected to sql db %s", conf.MariaConnection)
 	if conf.TimeProfiling {
-		conf.Logger.LogDebug("TIME_PROFILING: created sql connection in %sms",t1.StringMilisec())
+		conf.Logger.LogDebug("TIME_PROFILING: created sql connection in %sms", t1.StringMilisec())
 	}
 
 	// ELASTIC SEARCH
-	t2 :=  chronos.New()
+	t2 := chronos.New()
 	ctx := context.Background()
 	// Create a client
-	esClient, err := elastic.NewClient()
+	esClient, err := elastic.NewClient(elastic.SetURL(conf.ElasticConnection))
+
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create elasticsearch connection")
 	}
 	// check connection
-	_ , _ , err = esClient.Ping("/").Do(ctx)
+	_, _, err = esClient.Ping("/").Do(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to ping elasticsearch")
 	}
@@ -96,15 +97,15 @@ func New(conf Config) (*Client, error) {
 	t2.Finish()
 	conf.Logger.Log("successfully connected to elasticsearch db %s", conf.ElasticConnection)
 	if conf.TimeProfiling {
-		conf.Logger.LogDebug("TIME_PROFILING: created elasticsearch connection in %sms",t1.StringMilisec())
+		conf.Logger.LogDebug("TIME_PROFILING: created elasticsearch connection in %sms", t2.StringMilisec())
 	}
 
 	// init client
 	newClient := &Client{
-		esClient: esClient,
+		esClient:  esClient,
 		sqlClient: sqlClient,
 
-		ctx:ctx,
+		ctx:           ctx,
 		logger:        conf.Logger,
 		timeProfiling: conf.TimeProfiling,
 	}
