cd9e29c62a84b08e0d21a963e14b5d6772d759c2
Vaclav Rozsypalek <vaclav@giantswarm.io>
2018-12-31 12:46:46 +0100

    add debug

--- database/multi/cache/getUserNotificationSettings.go
+++ database/multi/cache/getUserNotificationSettings.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/exmonitor/exclient/database/spec/notification"
+	"fmt"
 )
 
 type SQL_GetUsersNotificationSetting struct {
@@ -20,13 +21,17 @@ type SQL_GetUsersNotificationSetting_Record struct {
 func (s *SQL_GetUsersNotificationSetting) IsCacheValid(serviceID int, ttl time.Duration) bool {
 	if r, ok := s.Cache[serviceID]; ok {
 		if r.Age.IsZero() {
-			// cache age is not set, cache is not balid
+			// cache age is not set, cache is not valid
+			fmt.Printf("cache is not valid, age is zero  for SQL_GetUsersNotificationSetting:ID: %d", serviceID)
+
 			return false
 		} else {
 			return time.Now().After(r.Age.Add(ttl))
 		}
 	} else {
 		// no cache for this record, so cache is not valid
+		fmt.Printf("cache expired  for SQL_GetUsersNotificationSetting:ID: %d", serviceID)
+
 		return false
 	}
 }
@@ -37,6 +42,7 @@ func (s *SQL_GetUsersNotificationSetting) GetData(serviceID int) []*notification
 		return d.Data
 	}
 	// cached data not found
+	fmt.Printf("no cache found for SQL_GetUsersNotificationSetting:ID: %d", serviceID)
 	return nil
 }
 
@@ -47,4 +53,6 @@ func (s *SQL_GetUsersNotificationSetting) CacheData(serviceID int, d []*notifica
 		Data: d,
 	}
 	s.Cache[serviceID] = r
+	fmt.Printf("saving cache for SQL_GetUsersNotificationSetting:ID: %d", serviceID)
+
 }
