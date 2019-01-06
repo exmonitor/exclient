285ea6272f696d647fdd94af57262f04f24676f0
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-01-06 18:13:20 +0100

    fix index creation

--- database/spec/status/aggregated.go
+++ database/spec/status/aggregated.go
@@ -1,6 +1,9 @@
 package status
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type AgregatedServiceStatus struct {
 	Id            string        `json:"id"`
@@ -11,4 +14,12 @@ type AgregatedServiceStatus struct {
 	Result        bool          `json:"result"`
 	TimestampFrom time.Time     `json:"@timestamp_from"`
 	TimestampTo   time.Time     `json:"@timestamp_to"`
+}
+
+func (a *AgregatedServiceStatus) String() string {
+	return fmt.Sprintf("[id:%s, serviceID%d, interval:%d, aggregated: %d, result: %t, from: %s, to: %s]", a.Id, a.ServiceID, a.Interval, a.Aggregated, a.Result, simpleTimeFormat(a.TimestampFrom), simpleTimeFormat(a.TimestampTo))
+}
+
+func simpleTimeFormat(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
 }
